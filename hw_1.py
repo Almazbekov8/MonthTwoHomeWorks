91c468dc2e822c221793ca3bb63fa48de5d24179
Aziret Almazbekov <izmeniziznzagod@gmail.com>
2025-09-02 21:20:34 +0600

    Создал объекты на основе класса Person

--- hw_1.py
+++ hw_1.py
@@ -6,6 +6,13 @@ class Person:
         self.higher_education = higher_education
 
     def introduce(self):
-        print(f"Привет меня зовут {self.name}, я родился в {self.birth_date}, я {self.occupation}" +
-              ", у меня есть высшее образование" if self.higher_education else "у меня нет высшего образование" )
+        print(f"Привет меня зовут {self.name}, я родился в {self.birth_date}, я {self.occupation},",
+              "у меня есть высшее образование" if self.higher_education else "у меня нет высшего образование" )
 
+person1 = Person("Адилет", "11.09.1990", "Водитель", True)
+person2 = Person("Нурсултан", "09.08.2007", "Юрист", True)
+person3 = Person("Арсен", "21.05.2006", "Бариста", False)
+
+person1.introduce()
+person2.introduce()
+person3.introduce()
