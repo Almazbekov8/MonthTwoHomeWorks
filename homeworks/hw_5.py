59bb0fdf0831e6c65080a0386eea9b3eddfd7dd9
Aziret Almazbekov <izmeniziznzagod@gmail.com>
2025-09-19 19:11:01 +0600

    Создал print для проверки всех магических методов

--- homeworks/hw_5.py
+++ homeworks/hw_5.py
@@ -3,3 +3,19 @@ from homeworks.distance import Distance
 d1 = Distance(10, "m")
 d2 = Distance(2, "km")
 d3 = Distance(500, "cm")
+
+
+print(d1)
+print(d2)
+print(d3)
+
+print(d1 + d2)
+print(d1 + d3)
+
+print(d2 - d1)
+print(d1 - d2)
+
+print(d1 == Distance(1000, "cm"))
+print(d1 < d2)
+
+print(d1.convert("km"))
