8f7d0f2bd0e6f11de623a972001ad0451c6a9976
Aziret Almazbekov <izmeniziznzagod@gmail.com>
2025-09-19 19:02:15 +0600

    Создал конструтор и магический метод __str__

--- homeworks/distance.py
+++ homeworks/distance.py
@@ -1,3 +1,9 @@
 class Distance:
     units = {"cm": 0.01, "m": 1, "km": 1000}
 
+    def __init__(self, value, unit):
+        self.value = value
+        self.unit = unit
+
+    def __str__(self):
+        return f"{self.value} {self.unit}"
