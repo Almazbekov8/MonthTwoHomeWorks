198386c41617064f81b32a16ffc36ad5b3fdc242
Aziret Almazbekov <izmeniziznzagod@gmail.com>
2025-09-19 19:05:29 +0600

    Создал два магических метода для сложения и вычетания

--- homeworks/distance.py
+++ homeworks/distance.py
@@ -5,8 +5,20 @@ class Distance:
         self.value = value
         self.unit = unit
 
+
     def __str__(self):
         return f"{self.value} {self.unit}"
 
     def to_meters(self):
         return self.value * self.units[self.unit]
+
+
+    def __add__(self, other):
+        total = self.to_meters() + other.to_meters()
+        return Distance(total / self.units[self.unit], self.unit)
+
+    def __sub__(self, other):
+        result = self.to_meters() - other.to_meters()
+        if result < 0:
+            return "Значение не может быть отрицательным"
+        return Distance(result / self.units[self.unit], self.unit)
