e91e6dccb4b002bac79c021ae4df9859c5d5146f
Aziret Almazbekov <izmeniziznzagod@gmail.com>
2025-09-29 21:09:02 +0600

    Создал декоратор для вывода времени

--- homeworks/hw_6.py
+++ homeworks/hw_6.py
@@ -2,4 +2,10 @@ from datetime import datetime as dt
 
 func_time = dt.now()
 
+def printer(func):
+    def wrapper(*args, **kwargs):
+        print(f"Время вызова функции {func_time.strftime('%H:%M:%S')}, {func.strftime("%d/%m/%Y")}")
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
 
